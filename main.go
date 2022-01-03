dec385751263eac1e533a2f0d30472132c3794b8
dumh <dumh>
2022-01-03 16:52:06 +0800

    1. add new json test

--- main.go
+++ main.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
 	"github.com/gagliardetto/solana-go/rpc"
+	jsoniter "github.com/json-iterator/go"
 	"time"
 )
 
@@ -24,4 +26,40 @@ func main() {
 	}
 	fmt.Printf("blockhash %s, Use time:%s\n", blockhash.Value.Blockhash, time.Now().Sub(start))
 	fmt.Println("End....")
+
+	jsonMap := make(map[string]string)
+	for i := 0; i < 100; i++ {
+		k := fmt.Sprint(i)
+		v := fmt.Sprintf("%d @#@!$!@#$!@#$@!#$!@#$DFQERQWERWQERQWERWQER", i)
+		jsonMap[k] = v
+	}
+	jMStart := time.Now()
+	jsonByte, err := json.Marshal(jsonMap)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("encoding/json Marshal :", time.Now().Sub(jMStart))
+
+	jUStart := time.Now()
+	err = json.Unmarshal(jsonByte, &jsonMap)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("encoding/json Unmarshal :", time.Now().Sub(jUStart))
+
+	var json2 = jsoniter.ConfigCompatibleWithStandardLibrary
+
+	j2MStart := time.Now()
+	jsonByte, err = json2.Marshal(jsonMap)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("jsoniter Marshal :", time.Now().Sub(j2MStart))
+
+	j2UStart := time.Now()
+	err = json2.Unmarshal(jsonByte, &jsonMap)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("jsoniter Unmarshal :", time.Now().Sub(j2UStart))
 }
